1e42c71c8f28d24fde0d57d3c4ad50d448981574
Jonathan Choi <jonchoi11@gmail.com>
2021-07-29 23:17:59 +0000

    fetching ip and coordinates

---
+++ index.js
@@ -0,0 +1,22 @@
+////////////////////////// fetchMyIP
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+//////////////////////////// fetchCoordsByIP
+// const { fetchCoordsByIp } = require('./iss');
+
+// fetchCoordsByIp('198.166.81.235', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
---
+++ iss.js
@@ -0,0 +1,53 @@
+const request = require("request");
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    let myIp = data.ip;
+
+    callback(null, myIp);
+  });
+};
+
+const fetchCoordsByIp = function(ip, callback) {
+  request('https://freegeoip.app/json/' + ip, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    let coordinates = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+    callback(null, coordinates);
+  });
+};
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIp };
